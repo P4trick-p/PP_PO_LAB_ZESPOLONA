c87748e3da4a9a503863857dc21e8f22c928fbc5
P4trick <patryk.pck98@gmail.com>
2018-04-12 14:21:25 +0200

    PP

--- PP_PO_LAB_ZESPOLONA/PP_PO_LAB_ZESPOLONA/Source.cpp
+++ PP_PO_LAB_ZESPOLONA/PP_PO_LAB_ZESPOLONA/Source.cpp
@@ -21,12 +21,14 @@ public:
 		Im = I;
 	}
 
-	Zespolona(int z = 0)
+	Zespolona(int z = 100)
 	{
 		srand(time(NULL));
 		Rm = (rand() % z) - (z / 2);
 		Im = (rand() % z) - (z / 2);
 	}
+
+	
 
 	~Zespolona()
 	{
@@ -69,25 +71,25 @@ public:
 		cout << endl;
 	};
 
-	double dodawanie(Zespolona z)
+	double dodawanie(double Rm2, double Im2)
 	{
-		z.Rm += Rm;
-		z.Im += Im;
-		return z.Rm, z.Im;
+		Rm += Rm2;
+		Im += Im2;
+		return Rm, Im;
 	};
 
-	double odejmowanie(Zespolona z)
+	double odejmowanie(double Rm2, double Im2)
 	{
-		z.Rm -= Rm;
-		z.Im -= Im;
-		return z.Rm, z.Im;
+		Rm -= Rm2;
+		Im -= Im2;
+		return Rm, Im;
 	};
 
-	double mnozenie(Zespolona z)
+	double mnozenie(double Rm2, double Im2)
 	{
-		z.Rm = z.Rm * Rm;
-		z.Im = z.Im * Im;
-		return z.Rm, z.Im;
+		Rm = (Rm*Rm2)-(Im*Im2);
+		Im = (Rm*Im2)+(Im*Rm2);
+		return Rm, Im;
 	};
 
 	void wyswietlanie()
@@ -117,59 +119,111 @@ public:
 		system("pause");
 	};
 
+	double copy(double r, double u) {
+		 r = Rm;
+		 u = Im;
+		return r, u;
+	}
 };
 
 int main()
 {
 	cout << "++ Kalkulator liczb zespolonych ++" << endl;
-	cout << "1. Wlasne" << endl << "2. Losowe" << endl;
+	cout << "1. Wlasne" << endl << "2. Losowe /* Obecnie nie dziala! */" << endl << "3. Stworz tablice n-elementowa liczb zespolonych" << endl;
 
 	cout << "Wybierz opcje: ";
 	cin >> op;
 	system("cls");
 	switch (op) {
 	case 1:
-
+	{
 		double I;
 		double R;
-		cout << "Wprowadz liczbe rzeczywista: ";
-		cin >> R;
-		system("cls");
-		cout << "Wprowadz liczbe urojona: ";
-		cin >> I;
+		char i;
+		cout << "Wprowadz liczbe zespolona:" << endl;
+		cout << "z= ";
+		cin >> R >> I >> i;
+
+
 		system("cls");
 		{
 			Zespolona z1(R, I);
 			z1.wyswietlanie();
-			z1.sprzezenie();
 			z1.modul();
+			z1.sprzezenie();
+			double R2, I2;
+			cout << "Dodaj inna liczbe zespolona:" << endl;
+			cout << "z= ";
+			cin >> R2 >> I2 >> i;
+			z1.dodawanie(R2, I2);
+			z1.wyswietlanie();
+			cout << "Teraz odejmij inna liczbe zespolona:" << endl;
+			cout << "z= ";
+			cin >> R2 >> I2 >> i;
+			z1.odejmowanie(R2, I2);
+			z1.wyswietlanie();
+			cout << "A na koniec pomnoz przez:" << endl;
+			cout << "z= ";
+			cin >> R2 >> I2 >> i;
+			z1.mnozenie(R2, I2);
+			z1.wyswietlanie();
 		}
 
-		break;
+	}
+	break;
+
 	case 2:
+		//main();
+	
+	{
 		int zakres;
 		cout << "WprowadŸ zakres liczb: ";
 		cin >> zakres;
 		{
+			double r=0;
+			double u=0;
+			Zespolona z1(zakres);
+			z1.wyswietlanie();
+			z1.modul();
+			z1.sprzezenie();
+			cout << "Dodawanie: ";
 			Zespolona z2(zakres);
 			z2.wyswietlanie();
-			z2.sprzezenie();
-			z2.modul();
+			//z2.copy(r,u);
+			z1.dodawanie(z1,z2);
+			z1.wyswietlanie();
 		}
+	}
+	
 		break;
+	
+	case 3: {
+		//int ile=0;
+		cout << "Ilu elementowa ma byc tablica?" << endl << "n = ";
+		//cin >> ile;
+		//Zespolona z1[5];
+		//for (int i = 0; i < ile; i++) {		}
+	}
+		break;
+
 	default:
 		cout << "Nie ma takiej opcji";
 		Sleep(1500);
 		main();
 		break;
 	}
+	
 	//	menu();
+
+
 	system("pause");
 	return 0;
+	
 }
 
-/*
-void menu() {
+
+/*//// MENU NIE DZIALA!!! ////
+void menu(int op, double R, double I, int zakres) {
 	int men;
 	cout << "++ MENU ++" << endl;
 	cout << "1.Modul" << endl;
@@ -180,11 +234,81 @@ void menu() {
 	cout << "6.Wyswietlanie" << endl;
 	cout << "7.Wyjœcie" << endl;
 	cin >> men;
+
+	switch (op) {
+	case 1:
+	{
+		Zespolona z1(R, I);
+		switch (men) {
+		case 1:
+			z1.modul();
+			break;
+		case 2:
+			z1.sprzezenie();
+			break;
+		case 3:
+			z1.dodawanie();
+			break;
+		case 4:
+			z1.odejmowanie();
+			break;
+		case 5:
+			z1.mnozenie();
+			break;
+		case 6:
+			z1.wyswietlanie
+			break;
+		case 7:
+			~Zespolona;
+			exit(0);
+			break;
+	}
+	break;
+	case 2:
+		Zespolona z2(zakres);
+		switch (men) {
+		case 1:
+			z2.modul();
+			break;
+		case 2:
+			z2.sprzezenie();
+			break;
+		case 3:
+			z2.dodawanie();
+			break;
+		case 4:
+			z2.odejmowanie();
+			break;
+		case 5:
+			z2.mnozenie();
+			break;
+		case 6:
+			z2.wyswietlanie
+				break;
+		case 7:
+			~Zespolona;
+			exit(0);
+			break;
+	break;
+	
+
+	/*
 	switch (men) {
 	case 1:
 	{
-		if (op == 1) { z1.modul(); }
-		if (op == 2) { z2.modul(); }
+		switch (op) {
+		case 1:
+			Zespolona z1;
+			z1.modul(); 
+			break;
+		case 2:
+			{ z2.modul(); }
+			break;
+		}
+
+		
+		//if (op == 1) { z1.modul(); }
+		//if (op == 2) { z2.modul(); }
 	}
 	break;
 	case 2:
@@ -228,5 +352,6 @@ void menu() {
 	menu();
 	break;
 	}
+	
 	}
 	*/
